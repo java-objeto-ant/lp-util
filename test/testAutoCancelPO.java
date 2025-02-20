3c36a4121d6abe51c6322b1be9d98bb385b0e06c
MayNevVal <97944690+mnv121@users.noreply.github.com>
2025-02-20 10:52:04 +0800

    lp auto cancel po

---
+++ test/testAutoCancelPO.java
@@ -0,0 +1,18 @@
+
+import org.rmj.lp.util.AutoCancelPO;
+
+public class testAutoCancelPO {
+
+//w/ configuration test
+    public static void main(String[] args) {
+        String[] arr = {"30"};
+
+        AutoCancelPO.main(arr);
+    }
+    
+    
+////no configuration test
+//    public static void main(String[] args) {
+//        AutoCancelPO.main(args);
+//    }
+}
